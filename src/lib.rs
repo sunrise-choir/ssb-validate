9b4e1b912ec15062298637bbce9adf007d6c7437
Piet Geursen <pietgeursen@gmail.com>
2019-11-09 15:41:06 +0100

    First passing tests!

--- src/lib.rs
+++ src/lib.rs
@@ -43,8 +43,10 @@ pub enum Error {
     InvalidMessageCouldNotSerializeValue{source: EncodeJsonError},
     #[snafu(display("The actual hash of the value did not match the hash claimed by `key`"))]
     ActualHashDidNotMatchKey,
-    #[snafu(display("`message.value.previous` did not match the `key` of previous message"))] 
-    PreviousWasNotCorrect,
+    #[snafu(display("Previous was set to null but it should have had a value"))]
+    PreviousWasNull,
+    #[snafu(display("This feed is forked. Last known good message was as seq: {}", previous_seq))] 
+    ForkedFeed{previous_seq: u64},
 }
 
 type Result<T, E = Error> = std::result::Result<T, E>;
@@ -63,6 +65,18 @@ struct SsbMessage {
     value: SsbMessageValue,
 }
 
+/// Check that a message is a valid relative to it's previous message. 
+///
+/// This checks that:
+/// - the sequence starts at one if it's the first message
+/// - the previous is correctly set to null if it's the first message
+/// - the sequence increments correctly
+/// - the author has not changed
+/// - the feed is not forked
+/// - the _actual_ hash matches the hash claimed in `key`
+///
+/// This does not check:
+/// - the signature. See ssb-verify-signatures which lets you to batch verification of signatures. 
 pub fn validate_hash_chain(message_bytes: &[u8], previous_msg_bytes: Option<&[u8]>) -> Result<()> {
     // msg seq is 1 larger than previous
     let previous_message = match previous_msg_bytes {
@@ -102,7 +116,7 @@ pub fn validate_hash_chain(message_bytes: &[u8], previous_msg_bytes: Option<&[u8
         );
         
         // msg previous must match hash of previous.value otherwise it's a fork.
-        ensure!(message.value.previous.context(PreviousWasNotCorrect)? == previous.key, PreviousWasNotCorrect);
+        ensure!(message.value.previous.context(PreviousWasNull)? == previous.key, ForkedFeed{previous_seq: previous.value.sequence});
         
     } else {
         //This message is the first message.
@@ -149,8 +163,56 @@ pub fn validate_hash_chain(message_bytes: &[u8], previous_msg_bytes: Option<&[u8
 
 #[cfg(test)]
 mod tests {
+    use crate::validate_hash_chain;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn it_works_first_message() {
+        assert!(validate_hash_chain(MESSAGE_1.as_bytes(), None).is_ok());
     }
+    #[test]
+    fn it_works_second_message() {
+        assert!(validate_hash_chain(MESSAGE_2.as_bytes(), Some(MESSAGE_1.as_bytes())).is_ok());
+    }
+
+    const MESSAGE_1: &str = r##"{
+  "key": "%/v5mCnV/kmnVtnF3zXtD4tbzoEQo4kRq/0d/bgxP1WI=.sha256",
+  "value": {
+    "previous": null,
+    "author": "@U5GvOKP/YUza9k53DSXxT0mk3PIrnyAmessvNfZl5E0=.ed25519",
+    "sequence": 1,
+    "timestamp": 1470186877575,
+    "hash": "sha256",
+    "content": {
+      "type": "about",
+      "about": "@U5GvOKP/YUza9k53DSXxT0mk3PIrnyAmessvNfZl5E0=.ed25519",
+      "name": "Piet"
+    },
+    "signature": "QJKWui3oyK6r5dH13xHkEVFhfMZDTXfK2tW21nyfheFClSf69yYK77Itj1BGcOimZ16pj9u3tMArLUCGSscqCQ==.sig.ed25519"
+  },
+  "timestamp": 1571140551481
+}"##;
+
+    const MESSAGE_2: &str = r##"{
+  "key": "%kLWDux4wCG+OdQWAHnpBGzGlCehqMLfgLbzlKCvgesU=.sha256",
+  "value": {
+    "previous": "%/v5mCnV/kmnVtnF3zXtD4tbzoEQo4kRq/0d/bgxP1WI=.sha256",
+    "author": "@U5GvOKP/YUza9k53DSXxT0mk3PIrnyAmessvNfZl5E0=.ed25519",
+    "sequence": 2,
+    "timestamp": 1470187292812,
+    "hash": "sha256",
+    "content": {
+      "type": "about",
+      "about": "@U5GvOKP/YUza9k53DSXxT0mk3PIrnyAmessvNfZl5E0=.ed25519",
+      "image": {
+        "link": "&MxwsfZoq7X6oqnEX/TWIlAqd6S+jsUA6T1hqZYdl7RM=.sha256",
+        "size": 642763,
+        "type": "image/png",
+        "width": 512,
+        "height": 512
+      }
+    },
+    "signature": "j3C7Us3JDnSUseF4ycRB0dTMs0xC6NAriAFtJWvx2uyz0K4zSj6XL8YA4BVqv+AHgo08+HxXGrpJlZ3ADwNnDw==.sig.ed25519"
+  },
+  "timestamp": 1571140551485
+}"##;
 }
