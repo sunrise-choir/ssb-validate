89ef544039bd490be3eb1544e34d7052f8a9c344
Piet Geursen <pietgeursen@gmail.com>
2019-11-14 20:29:34 +0100

    proof reading.

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,10 @@
 //! Secure Scuttlebutt "feeds" are a sequence of messages published by one author.
 //! To be a valid message,
 //! - each message must include the hash of the preceding message
-//! - the sequence number must be one larger than the preceding message
-//! - the author must not change compared to the last preceding message
+//! - the sequence number must be one larger than sequence of the preceding message
+//! - the author must not change compared to the preceding message
 //! - If it's the first message in a feed, the sequence must be 1 and the previous must be null.
+//! - If the message includes the key, it must be that hash of the value of the message..
 //!
 //! You can check messages one by one or batch process a collection of them (uses [rayon](https://docs.rs/rayon/1.2.0/rayon/index.html) internally)
 //!
